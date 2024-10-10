36be9a956fff2febd35a61d676f447238df22091
chaim <chaim.triebitz@bhbsdventures.com>
2024-10-10 14:49:44 +0300

    kippur

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react'
-
-export const Home = () => {
-   return (
-      <div>
-        <h1>Home sweet home</h1>
-      </div>
-   )
-}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-import { Home } from './Home.jsx';
 import { Form } from './Form.jsx';
 import { Banks } from './Banks.jsx';
 
 export {
-   Home,
    Form,
    Banks
 }
